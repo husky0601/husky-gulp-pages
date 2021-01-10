9a0b7092485bd78bb9e7419cf3dc45075660f4a6
husky0601 <suyating228@gmail.com>
2021-01-10 10:23:40 +0800

    feat: husky-gulp-pages

---
+++ example/pages.config.js
@@ -0,0 +1,57 @@
+// 抽象公共数据成员
+
+module.exports={
+    build:{
+        src: 'src',
+        dist: 'dist',
+        temp: '.tmp',
+        public: 'public',
+        paths: {
+          styles: 'assets/styles/*.scss',
+          scripts: 'assets/scripts/*.js',
+          pages: '*.html',
+          images: 'assets/images/**',
+          fonts: 'assets/fonts/**'
+        }
+    },
+    data : {
+        menus: [
+          {
+            name: 'Home',
+            icon: 'aperture',
+            link: 'index.html'
+          },
+          {
+            name: 'Features',
+            link: 'features.html'
+          },
+          {
+            name: 'About',
+            link: 'about.html'
+          },
+          {
+            name: 'Contact',
+            link: '#',
+            children: [
+              {
+                name: 'Twitter',
+                link: 'https://twitter.com/w_zce'
+              },
+              {
+                name: 'About',
+                link: 'https://weibo.com/zceme'
+              },
+              {
+                name: 'divider'
+              },
+              {
+                name: 'About',
+                link: 'https://github.com/husky0601'
+              }
+            ]
+          }
+        ],
+        pkg: require('./package.json'),
+        date: new Date()
+    }
+}
